bbccfeac9dc426e8eb8841852dbebe42bf9560d6
Theo Berkau <cwx@cyberwarriorx.com>
2013-02-07 02:31:23 -0500

    More cmake fixes

--- install.bat
+++
@@ -1,3 +0,0 @@
-copy libiapetus%1.a "%PPATH%\KPIT Cummins\GNUSH%kpitver%-%kpitoutput%\sh-%kpitoutput%\lib"
-md "%PPATH%\KPIT Cummins\GNUSH%kpitver%-%kpitoutput%\sh-%kpitoutput%\sh-%kpitoutput%\include\iapetus"
-copy src\*.h "%PPATH%\KPIT Cummins\GNUSH%kpitver%-%kpitoutput%\sh-%kpitoutput%\sh-%kpitoutput%\include\iapetus"
